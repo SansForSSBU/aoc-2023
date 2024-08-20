a4f83c663809daf3412dc9536929b772fa2d3381
Joseph Taylor <taylorjoseph3333@gmail.com>
2024-08-20 23:36:35 +0100

    Finish puzzle 2

--- puzzle2/solution.py
+++ puzzle2/solution.py
@@ -4,28 +4,34 @@ maxes = {
     "green": 13,
     "blue": 14
 }
-ans = 0
+pt1_ans = 0
+pt2_ans = 0
 with open("puzzle2/input.txt") as input:
     games = input.read().split("\n")
     for game in games:
         game_possible = True
         game_name = game.split(":")[0]
         game_info = game.split(":")[1].strip().split(";")
-        total_balls = {
+        max_balls = {
             "red": 0,
             "green": 0,
             "blue": 0
         }
+
         for show in game_info:
             balls = [x.strip().split(" ") for x in show.split(",")]
             for ball in balls:
-                total_balls[ball[1]] += int(ball[0])
                 if int(ball[0]) > maxes[ball[1]]:
                     game_possible = False
-        #for key, value in total_balls.items():
-            #if maxes[key] < value:
-            #    game_possible = False
-        if game_possible:
-            ans += int(game_name.split(" ")[1])
+                max_balls[ball[1]] = max(max_balls[ball[1]], int(ball[0]))
 
-print(ans)
+        if game_possible:
+            pt1_ans += int(game_name.split(" ")[1])
+        
+        power = 1
+        for k, v in max_balls.items():
+            power = power * v
+        pt2_ans += power
+
+print("Part 1: ", pt1_ans)
+print("Part 2: ", pt2_ans)
