8a798ea021fa9309059c06c5f606e8a533e79805
Joseph Taylor <taylorjoseph3333@gmail.com>
2024-08-20 22:53:58 +0100

    Solve puzzle1 pt2

--- puzzle1/solution.py
+++ puzzle1/solution.py
@@ -1,21 +1,45 @@
+import math
+
+translation = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+}
+def interpret(num):
+    if translation.get(num) != None:
+        return translation.get(num)
+    elif translation.get(num[::-1]) != None:
+        return translation.get(num[::-1])
+    return num
+
+def find_first_number(line, nums):
+    lowest_index = math.inf
+    first_num = "0"
+    for num in nums:
+        idx = line.find(num)
+        if (idx == -1):
+            continue
+        if idx <= lowest_index:
+            first_num = interpret(num)
+            lowest_index = idx
+    return first_num
+
 total = 0
-with open("input") as f:
+nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+reversed_nums = [num[::-1] for num in nums]
+
+with open("puzzle1/input") as f:
     lines=f.read().split("\n")
     for line in lines:
-        first = "0"
-        last = "0"
-        for i in range(len(line)):
-            if line[i].isdigit():
-                first = line[i]
-                break
-
-        for i in range(len(line)):
-            if line[-(i+1)].isdigit():
-                last = line[-(i+1)]
-                break
+        first = find_first_number(line, nums)
+        last = find_first_number(line[::-1], reversed_nums)
         num = int(first+last)
-        print(line)
-        print(num)
         total += num
 
 print(total)
