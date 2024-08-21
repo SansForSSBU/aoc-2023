149d41aa0c206100db8d86ff1fd58eb5d15aed39
Joseph Taylor <taylorjoseph3333@gmail.com>
2024-08-21 01:46:06 +0100

    Create solution template

--- setup_puzzle.sh
+++ setup_puzzle.sh
@@ -1,3 +1,3 @@
 mkdir puzzle$1
 touch ./puzzle$1/input.txt
-touch ./puzzle$1/solution.py
+cp ./template.py ./puzzle$1/solution.py
---
+++ template.py
@@ -0,0 +1,12 @@
+
+lines = []
+with open("puzzle/input.txt") as input:
+    lines = input.read().split("\n")
+
+
+
+
+def solve_pt1():
+    return 0
+def solve_pt2():
+    return 0
