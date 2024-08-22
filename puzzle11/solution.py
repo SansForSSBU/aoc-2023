d12defc1ce01d5e2d182e3b9813e97734e854f94
Joseph Taylor <taylorjoseph3333@gmail.com>
2024-08-22 21:26:19 +0100

    Solved puzzle 11

--- puzzle11/solution.py
+++ puzzle11/solution.py
@@ -10,20 +10,11 @@ def get_column(map, x):
 def is_empty(line):
     return line.count(".") == len(line)
 
-def insert_column(map, pos, column):
-    for i, line in enumerate(map):
-        new_line = line[:pos] + column[i] + line[pos:]
-        map[i] = new_line
-
-def expand_empty_lines(map):
+def get_empty_lines(map):
     empty_lines = []
-    empty_line_example = ""
     for y, line in enumerate(map):
         if is_empty(line):
             empty_lines.append(y)
-            empty_line_example = line
-    for y in reversed(empty_lines):
-        map.insert(y, empty_line_example)
 
     empty_columns = []
     empty_column_example = ""
@@ -31,10 +22,8 @@ def expand_empty_lines(map):
         column = get_column(map, x)
         if is_empty(column):
             empty_columns.append(x)
-            empty_column_example = column
-
-    for x in empty_columns:
-        insert_column(map, x, empty_column_example)
+    
+    return empty_lines,empty_columns
 
 def get_galaxy_coords(map):
     coords = []
@@ -44,31 +33,26 @@ def get_galaxy_coords(map):
                 coords.append((x, y))
     return coords
 
-def taxicab_distance(point1, point2):
-    return abs(point1[0]-point2[0]) + abs(point1[1] - point2[1])
-
-def print_map(map):
-    for line in map:
-        print(line)
-
-def solve_pt1():
-
-    print(len(get_galaxy_coords(lines)))
-    expand_empty_lines(lines)
-    galaxy_coords = get_galaxy_coords(lines)
-    print(len(galaxy_coords))
-    result = 0
-    for n, point1 in enumerate(galaxy_coords):
-        if n == len(galaxy_coords) - 2:
-            print("Hello")
-        for point2 in galaxy_coords[n+1:]:
-            result += taxicab_distance(point1, point2)
+def taxicab_distance_part2(point1, point2, empty_rows, empty_columns, factor):
+    result = abs(point1[0]-point2[0]) + abs(point1[1] - point2[1])
+    x_crossed = range(min(point1[0], point2[0]), max(point1[0], point2[0]))
+    y_crossed = range(min(point1[1], point2[1]), max(point1[1], point2[1]))
+    for x in empty_columns:
+        if x in x_crossed:
+            result += (factor-1)
+    for y in empty_rows:
+        if y in y_crossed:
+            result += (factor-1)
     return result
 
-def solve_pt2():
-    return 0
+def solve_pt1(factor):
+    empty_rows, empty_columns = get_empty_lines(lines)
+    galaxy_coords = get_galaxy_coords(lines)
+    result = 0
+    for n, point1 in enumerate(galaxy_coords):
+        for point2 in galaxy_coords[n+1:]:
+            result += taxicab_distance_part2(point1, point2, empty_rows, empty_columns, factor)
+    return result
 
-print(solve_pt1())
-print(solve_pt2())
-
-
+print(solve_pt1(2))
+print(solve_pt1(1000000))
