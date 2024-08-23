df5a19de6ccb342bc353406d642062010dfc0b5b
Joseph Taylor <taylorjoseph3333@gmail.com>
2024-08-24 00:12:22 +0100

    Refactor

--- puzzle14/solution.py
+++ puzzle14/solution.py
@@ -1,6 +1,13 @@
+from enum import Enum
 lines = []
 with open("puzzle14/input.txt") as input:
     lines = input.read().split("\n")
+
+class D(Enum):
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
 
 def print_map(map):
     for line in map:
@@ -10,25 +17,32 @@ def get_column(map, x):
     column = "".join([line[x] for line in map])
     return column
 
-def find_next_y(map, coord):
-    if coord[1] > 30:
-        pass
-    l = get_column(map, coord[0])
-    y = max(l.rfind("O", 0, coord[1]), l.rfind("#", 0, coord[1])) + 1
-    return y
+def find_next_pos(map, coord, dir):
+    x = coord[0]
+    y = coord[1]
+    if dir == D.NORTH:
+        l = get_column(map, coord[0])
+        y = max(l.rfind("O", 0, coord[1]), l.rfind("#", 0, coord[1])) + 1
+    return (x, y)
 
 def set_coord(map, coord, char):
     map[coord[1]] = map[coord[1]][:coord[0]] + char + map[coord[1]][coord[0]+1:]
+
+def move_rock(map, coord, dir):
+    x = coord[0]
+    y = coord[1]
+    set_coord(map, (x, y), ".")
+    next_pos = find_next_pos(map, coord, dir)
+    set_coord(map, next_pos, "O")
+    return next_pos
 
 def solve_pt1():
     load = 0
     for y, l in enumerate(lines):
         for x, c in enumerate(l):
             if c == "O":
-                new_y = find_next_y(lines, (x, y))
-                set_coord(lines, (x, y), ".")
-                set_coord(lines, (x, new_y), "O")
-                load += len(lines) - new_y
+                new_pos = move_rock(lines, (x, y), D.NORTH)
+                load += len(lines) - new_pos[1]
     return load
 def solve_pt2():
     return 0
