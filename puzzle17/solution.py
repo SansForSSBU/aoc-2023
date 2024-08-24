1c665cae41ebd5fd0395e4542ff9b762ddc4e0e9
Joseph Taylor <taylorjoseph3333@gmail.com>
2024-08-24 10:06:35 +0100

    Solve part 1 (very inefficiently)

--- puzzle17/solution.py
+++ puzzle17/solution.py
@@ -60,12 +60,13 @@ class State:
             state = copy.deepcopy(self)
             state.move_1_tile()
             next_states.append(state)
-        for direction in LR_turns[self.dir]:
-            state = copy.deepcopy(self)
-            state.tiles_left = max_path_length
-            state.dir = direction
-            state.move_1_tile()
-            next_states.append(state)
+        if max_path_length - self.tiles_left >= min_path_length:
+            for direction in LR_turns[self.dir]:
+                state = copy.deepcopy(self)
+                state.tiles_left = max_path_length
+                state.dir = direction
+                state.move_1_tile()
+                next_states.append(state)
         next_states = [state for state in next_states if get_tile(map, state.coords) is not None]
         for state in next_states:
             state.cost += get_tile(map, state.coords)
@@ -74,32 +75,41 @@ class State:
         
         
 
-checked_states = []
-max_path_length = 3
+checked_states = {}
+def add_to_checked_states(state):
+    checked_states[state.coords] = checked_states.get(state.coords, []) + [state]
+def state_has_been_checked(state):
+    return state in checked_states.get(state.coords, [])
+
 def solve_pt1():
-    end_coords = (len(lines[0]), len(lines))
+    states_checked = 0
     states = [State((0, 0), d, max_path_length, 0) for d in [D.EAST, D.SOUTH]]
     while len(states) > 0:
         idx, state = min(enumerate(states), key=lambda x:x[1].cost)
         del states[idx]
-        if state in checked_states:
-            continue
-        checked_states.append(state)
-        if len(checked_states) % 1000 == 0:
-            print(len(checked_states))
+        add_to_checked_states(state)
+        if states_checked % 1000 == 0:
+            print(states_checked)
+        states_checked += 1
         next_states = state.get_next_states(lines)
         for state in next_states:
+            if state_has_been_checked(state):
+                continue
             duplicate = next((x for x in states if x == state), None)
             if duplicate is None:
                 states.append(state)
             else:
                 duplicate.cost = min(state.cost, duplicate.cost)
-        pass
-    end_state = next((state for state in states if state.coords == end_coords))
-    return end_state.cost
+            
+    end_cost = min([state.cost for state in checked_states[(140,140)]])
+    return end_cost
 
 def solve_pt2():
     return 0
 
+min_path_length = 1
+max_path_length = 3
 print("Part 1:", solve_pt1())
+min_path_length = 4
+max_path_length = 10
 print("Part 2:", solve_pt2())
