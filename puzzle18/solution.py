a070d27ba5370db7cf1ccda07180e700bd352fb2
Joseph Taylor <taylorjoseph3333@gmail.com>
2024-08-24 21:17:52 +0100

    WIP

--- puzzle18/solution.py
+++ puzzle18/solution.py
@@ -54,6 +54,15 @@ def get_tile(grid, coords):
 def set_coord(map, coord, char):
     map[coord[1]] = map[coord[1]][:coord[0]] + char + map[coord[1]][coord[0]+1:]
 
+def add_coords(coord1, coord2):
+    return (coord1[0]+coord2[0], coord1[1]+coord2[1])
+
+def sub_coords(coord1, coord2):
+    return (coord1[0]-coord2[0], coord1[1]-coord2[1])
+
+def multiply_coord(coord, factor):
+    return (coord[0]*factor, coord[1]*factor)
+
 def get_possible_expansions(map, coords):
     adjacent_tiles = get_adjacent_coords(coords)
     adjacent_tiles = [coord for coord in adjacent_tiles if get_tile(map, coord) == "."]
@@ -82,11 +91,28 @@ def solve_pt1(start_pos, grid_square_size):
         ans += line.count("#")
         ans += line.count(".")
     return ans
+
+def area_from_vertices(vertices):
+    #WIP
+    area = 0
+    x_positions = sorted(list(set(vertex[0] for vertex in vertices)))
+    return area
+
 def solve_pt2():
-    return 0
+    v1 = (0, 0)
+    vertices = [v1]
+    sides = []
+    for instr in lines:
+        direction = instr[0]
+        distance = instr[1]
+        direction_vector = multiply_coord(go_direction((0,0), direction), distance)
+        new_vertex = add_coords(vertices[-1], direction_vector)
+        vertices.append(new_vertex)
+    return area_from_vertices(vertices)
 
 
-grid_square_size = 800
-pos =  (int(grid_square_size/2), int(grid_square_size/2))
-print("Part 1:", solve_pt1(pos, grid_square_size))
+
+#grid_square_size = 800
+#pos =  (int(grid_square_size/2), int(grid_square_size/2))
+#print("Part 1:", solve_pt1(pos, grid_square_size))
 print("Part 2:", solve_pt2())
