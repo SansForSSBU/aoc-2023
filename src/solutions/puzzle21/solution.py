fba06833c95d4fe4ad7c71b95964e54bbdf4203c
SansForSSBU <taylorjoseph3333@gmail.com>
2026-01-08 21:31:03 +0000

    WIP

--- src/solutions/puzzle21/solution.py
+++ src/solutions/puzzle21/solution.py
@@ -1,27 +1,38 @@
 import numpy as np
-def get_adjacents(pos):
-    (x, y) = pos
-    adds = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-    for add in adds:
-        yield (x + add[0], y + add[1])
 
-def get_coords(grid, pos):
-    (x, y) = pos
-    if x > len(grid[0]) or x < 0:
-        return -1
-    if y > len(grid) or y < 0:
-        return -1
-    return grid[y][x]
+class Grid():
+    def __init__(self, grid):
+        self.grid = grid
+
+    def get_coords(self, pos):
+        (x, y) = pos
+        if x > len(self.grid[0]) or x < 0:
+            return -1
+        if y > len(self.grid) or y < 0:
+            return -1
+        return self.grid[y][x]
+    
+    def get_adjacents(self, pos):
+        (x, y) = pos
+        adds = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        for add in adds:
+            yield (x + add[0], y + add[1])
+
+
 
 def solve_pt1(grid, farmer_pos):
     positions = [farmer_pos]
     for i in range(64):
         next_positions = set([])
         for position in positions:
-            next_positions.update(get_adjacents(position))
-        positions = [pos for pos in list(next_positions) if get_coords(grid, pos) == 0]  
-        print(i, len(positions))
+            next_positions.update(grid.get_adjacents(position))
+        positions = [pos for pos in list(next_positions) if grid.get_coords(pos) == 0]  
     return len(positions)
+
+def solve_pt2(grid, farmer_pos):
+    positions = {farmer_pos: [(0, 0)]}
+    steps = 26501365
+
 
 def main(input_file):
     lines = input_file.split("\n")[:-1]
@@ -30,7 +41,7 @@ def main(input_file):
             x = line.find("S")
             farmer_pos = (x,y)
             break
-    grid = np.array([[1 if char == "#" else 0 for char in list(line)] for line in lines])
+    grid = Grid(np.array([[1 if char == "#" else 0 for char in list(line)] for line in lines]))
     
     pt1_ans = solve_pt1(grid, farmer_pos)
     return (pt1_ans, 0)
