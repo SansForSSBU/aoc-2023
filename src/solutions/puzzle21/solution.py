60fae19a89df48ad29157f98c0d0212f8c33b9d7
SansForSSBU <taylorjoseph3333@gmail.com>
2026-01-08 21:48:42 +0000

    WIP

--- src/solutions/puzzle21/solution.py
+++ src/solutions/puzzle21/solution.py
@@ -11,6 +11,26 @@ class Grid():
         if y > len(self.grid) or y < 0:
             return False
         return True
+    
+    def get_pu_offset(self, pos):
+        if self.is_in_grid(pos):
+            return (0,0)
+        (x, y) = pos
+        if x < 0:
+            return (-1,0)
+        if y < 0:
+            return (0,-1)
+        if x >= len(self.grid[0]):
+            return (1,0)
+        if y >= len(self.grid[1]):
+            return (0,1)
+        raise Exception()
+    
+    def snap_to_grid(self, pos):
+        (x, y) = pos
+        x = x % len(self.grid[0])
+        y = y % len(self.grid)
+        return (x,y)
 
     def get_coords(self, pos):
         (x, y) = pos
@@ -24,7 +44,8 @@ class Grid():
         for add in adds:
             yield (x + add[0], y + add[1])
 
-
+def add_positions(a,b):
+    return (a[0]+b[0], a[1]+b[1])
 
 def solve_pt1(grid, farmer_pos):
     positions = [farmer_pos]
@@ -36,8 +57,28 @@ def solve_pt1(grid, farmer_pos):
     return len(positions)
 
 def solve_pt2(grid, farmer_pos):
-    positions = {farmer_pos: [(0, 0)]}
+    positions = {
+        farmer_pos: set([(0, 0)])
+    }
     steps = 26501365
+    for i in range(steps):
+        next_positions = {}
+        for pos,PUs in positions.items():
+            adjacents = grid.get_adjacents(pos)
+            for adj in adjacents:
+                pu_offset = grid.get_pu_offset(adj)
+                contributions = [add_positions(pu, pu_offset) for pu in PUs]
+                existing = next_positions.get(adj, None)
+                if existing == None:
+                    existing = set([])
+                existing.update(contributions)
+                next_positions[adj] = existing
+                pass
+        positions = next_positions
+        if i % 1000 == 0:
+            print(i)
+    return sum(sum(len(v)) for v in positions.values())
+                
 
 
 def main(input_file):
@@ -50,4 +91,5 @@ def main(input_file):
     grid = Grid(np.array([[1 if char == "#" else 0 for char in list(line)] for line in lines]))
     
     pt1_ans = solve_pt1(grid, farmer_pos)
-    return (pt1_ans, 0)
+    pt2_ans = solve_pt2(grid, farmer_pos)
+    return (pt1_ans, pt2_ans)
