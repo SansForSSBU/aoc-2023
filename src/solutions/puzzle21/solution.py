498e8c658c04c189c8e40f8a908871607a3c4485
Joseph Taylor <taylorjoseph3333@gmail.com>
2026-04-10 19:27:05 +0100

    Rewrite part 1 solution

--- src/solutions/puzzle21/solution.py
+++ src/solutions/puzzle21/solution.py
@@ -13,21 +13,7 @@ class Grid():
             return False
         return True
     
-    def get_pu_offset(self, pos):
-        if self.is_in_grid(pos):
-            return (0,0)
-        (x, y) = pos
-        if x < 0:
-            return (-1,0)
-        if y < 0:
-            return (0,-1)
-        if x >= len(self.grid[0]):
-            return (1,0)
-        if y >= len(self.grid[1]):
-            return (0,1)
-        raise Exception()
-    
-    def snap_to_grid(self, pos):
+    def convert_to_grid_detection_pos(self, pos):
         (x, y) = pos
         x = x % len(self.grid[0])
         y = y % len(self.grid)
@@ -35,8 +21,6 @@ class Grid():
 
     def get_coords(self, pos):
         (x, y) = pos
-        if not self.is_in_grid(pos):
-            return -1
         return self.grid[y][x]
     
     def set_coords(self, pos, val):
@@ -50,145 +34,32 @@ class Grid():
         adds = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         for add in adds:
             extra_pos = (x + add[0], y + add[1])
-            if self.is_in_grid(extra_pos) and not self.is_rock(extra_pos):
+            if not self.is_rock(extra_pos):
                 yield extra_pos
 
     def is_rock(self, pos):
-        return self.get_coords(pos) == 1
+        grid_detection_pos = self.convert_to_grid_detection_pos(pos)
+        return self.get_coords(grid_detection_pos) == 1
 
 def add_positions(a,b):
     return (a[0]+b[0], a[1]+b[1])
 
-def count_occ(grid):
-    grid = Grid(grid)
-    even_occupancy = 0
-    odd_occupancy = 0
-    for x in range(len(grid.grid[0])):
-        for y in range(len(grid.grid)):
-            if grid.get_coords((x,y)) == 1:
-                if sum([x,y]) % 2 == 0:
-                    even_occupancy += 1
-                else:
-                    odd_occupancy += 1
-    return (even_occupancy, odd_occupancy)
-
-def get_occupancy(grid, farmer_pos, n_steps):
-    occupancy = Grid(np.zeros_like(grid.grid))
-    occupancy.set_coords(farmer_pos, 1)
-    to_check = [farmer_pos]
-    for i in range(n_steps):
-        next_to_check = set()
-        for pos in to_check:
-            for adj in grid.get_adjacents(pos):
-                if grid.get_coords(adj) == 0:
-                    next_to_check.add(adj)
-                    occupancy.set_coords(adj, 1)
-        to_check = list(next_to_check)
-
-    (even_occupancy, odd_occupancy) = count_occ(occupancy.grid)
-    return even_occupancy, odd_occupancy, occupancy
-
-def solve_pt1(grid, farmer_pos, n_steps):
-    (even_occupancy, odd_occupancy, _) = get_occupancy(grid, farmer_pos, n_steps)
-    if (sum(farmer_pos)+n_steps) % 2 == 0:
-        return even_occupancy
-    else:
-        return odd_occupancy
-
-def n_grids(n_field_steps, occupancies, n_steps):
-    n_field_steps = n_field_steps
-    ans = 0
-    even_fields = 1
-    odd_fields = 0
-    for n in range(1,n_field_steps):
-        idx = (n+n_steps) % 2
-        ans += 4 * n * occupancies["C"][idx]
-    
-    n = n_field_steps
-    idx = (n+n_steps) % 2
-    ans += occupancies["N"][idx] + occupancies["E"][idx] + occupancies["S"][idx] + occupancies["W"][idx]
-    ans += (occupancies["NE"][idx] + occupancies["SE"][idx] + occupancies["SW"][idx] + occupancies["NW"][idx]) * (n-4)
-    return ans
-    #return 2*n_field_steps*n_field_steps + 2*n_field_steps + 1
-
-def detile(tiled, n=3):
-    h = tiled.shape[0] // n
-    w = tiled.shape[1] // n
-    return (
-        tiled
-        .reshape(n, h, n, w)
-        .swapaxes(1, 2)
-        .reshape(n*n, h, w)
-    )
-
-def swap(tup):
-    return (tup[1], tup[0])
-
-def solve_pt2(grid, farmer_pos, n_steps=26501365):
-    # 637531791816968 too low
-    # 637525510428520 too low
-    # 637531813260100 too low
-    # 637538093084305 wrong
-    # 637538093080475 wrong
-    # 637538116142463 wrong
-    # 637538093080263 wrong
-    # 637538116142675 wrong
-    # 637537318244157 wrong
-    # 637537318247987 wrong
-    # 637531814878787 wrong
-    # 637531790190315 wrong
-    # First, just think about the spaces that can be reached.
-    
-    n_field_steps = n_steps // 131
-    # Positions: Key is the direction you come from.
-    big_grid = Grid(np.tile(grid.grid, (5,5)))
-    _, _, occ = get_occupancy(big_grid, add_positions(farmer_pos, (262, 262)), n_steps = 131 * min(n_field_steps, min(n_field_steps, 1)) + (n_steps % 131))
-    detiled = detile(occ.grid, n=5)
-    things = {
-        "NW": detiled[6],
-        "NN": detiled[2],
-        "N": detiled[7],
-        "NE": detiled[8],
-        "WW": detiled[10],
-        "W": detiled[11],
-        "C": detiled[12],
-        "EE": detiled[14],
-        "E": detiled[13],
-        "SW": detiled[16],
-        "S": detiled[17],
-        "SS": detiled[22],
-        "SE": detiled[18]
-    }
-    for k in things.keys():
-        things[k] = count_occ(things[k])
-    
-    flood = things["C"]
-
-    ans = 0
-    ans += flood[n_steps%2]
-    # Because steps is odd, but fields crossed is even and this is n+1 these should be the even cases
-    ans += things["N"][(n_steps+1)%2]
-    ans += things["E"][(n_steps+1)%2]
-    ans += things["S"][(n_steps+1)%2]
-    ans += things["W"][(n_steps+1)%2]
-    ans += things["NN"][(n_steps)%2]
-    ans += things["EE"][(n_steps)%2]
-    ans += things["SS"][(n_steps)%2]
-    ans += things["WW"][(n_steps)%2]
-    ans += things["NE"][(n_steps)%2] * (n_field_steps)
-    ans += things["NW"][(n_steps)%2] * (n_field_steps)
-    ans += things["SE"][(n_steps)%2] * (n_field_steps)
-    ans += things["SW"][(n_steps)%2] * (n_field_steps)
-
-    # C (odd)
-    # even/odd are in terms of steps from the origin
-    state = n_steps%2
-    for i in range(1,n_field_steps):
-        state = (state + 1) % 2
-        ans += flood[state]*(i*4)
-    return ans
-                
-
+def solve_pt1(grid, farmer_pos, num_steps):
+    reached = np.zeros(grid.grid.shape)
+    reached[farmer_pos[1], farmer_pos[0]] = 1
+    positions = [farmer_pos]
+    steps_to = {farmer_pos: 0}
+    for step in range(1, num_steps+1):
+        next_positions = []
+        for position in positions:
+            for next in grid.get_adjacents(position):
+                if reached[next[1], next[0]] == 1:
+                    continue
+                reached[next[1], next[0]] = 1
+                steps_to[next] = min(step, steps_to.get(next, math.inf))
+                next_positions.append(next)
+        positions = next_positions
+    return len([k for k,v in steps_to.items() if v % 2 == 0])
 
 def main(input_file):
     lines = input_file.split("\n")[:-1]
