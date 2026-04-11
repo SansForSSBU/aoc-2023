d625c575ccec06f02585b9e8d2f410159a274d13
Joseph Taylor <taylorjoseph3333@gmail.com>
2026-04-11 15:07:26 +0100

    WIP

--- src/solutions/puzzle24/solution.py
+++ src/solutions/puzzle24/solution.py
@@ -1,40 +1,17 @@
 import math
+from sympy import symbols, linsolve, EmptySet
+
+
+t = symbols('t', real=True)
 
 class Particle():
-    def __init__(self, input_line):
+    def __init__(self, input_line):        
         pos, vel = input_line.split("@")
-        self.pos = [float(x) for x in pos.split(",")]
-        self.vel = [float(x) for x in vel.split(",")]
-
-    def pos_at_time(self, t):
-        x = self.pos[0] + (self.vel[0] * t)
-        y = self.pos[1] + (self.vel[1] * t)
-        z = self.pos[2] + (self.vel[2] * t)
-        return (x,y,z)
-
-
-    def intersects_at(self, other):
-        # Find at what t self.x = other.x
-        spx = self.pos[0]
-        opx = other.pos[0]
-        svx = self.vel[0]
-        ovx = other.vel[0]
-        pos_diff = opx - spx # How much distance is there to cover?
-        vel_diff = svx - ovx # How fast is self catching up to other?
-        if vel_diff == 0:
-            if int(pos_diff) == 0:
-                raise Exception()
-            return None
-        time = pos_diff / vel_diff
-        if time < 0:
-            return None
-        
-        self_at_time = self.pos_at_time(time)
-        other_at_time = other.pos_at_time(time)
-        if math.isclose(self_at_time[1], other_at_time[1], abs_tol=100000):
-            print(time)
-            return self_at_time
-        return None
+        p = [int(x) for x in pos.split(",")]
+        v = [int(x) for x in vel.split(",")]
+        self.x = t*v[0] + p[0]
+        self.y = t*v[1] + p[1]
+        self.z = t*v[2] + p[2]
 
 def is_in_test_area(pos):
     if pos[0] >= 200000000000000 and pos[0] <= 400000000000000:
@@ -47,12 +24,14 @@ def main(input_file):
     particles = [Particle(x) for x in input_file.split("\n") if len(x) != 0]
     for idx, p1 in enumerate(particles):
         for p2 in particles[idx+1:]:
-            intersect = p1.intersects_at(p2)
-            if intersect is None:
+            intersection = linsolve([p1.x - p2.x, p1.y - p2.y], (t))
+            if intersection == EmptySet:
                 continue
-            if is_in_test_area(intersect):
+            t_value = list(intersection)[0][0]
+            x = float(p1.x.subs(t, t_value))
+            y = float(p1.y.subs(t, t_value))
+            pos = (x,y)
+            if is_in_test_area(pos):
                 pt1_ans += 1
+    
     return (pt1_ans, 0)
-    # 1 wrong
-    # 0 wrong
-    pass
