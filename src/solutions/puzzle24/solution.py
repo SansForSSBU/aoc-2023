bc65b0a58f492375ac5cd7456761c2adc167e6e6
Joseph Taylor <taylorjoseph3333@gmail.com>
2026-04-11 14:21:30 +0100

    WIP

---
+++ src/solutions/puzzle24/solution.py
@@ -0,0 +1,53 @@
+import math
+
+class Particle():
+    def __init__(self, input_line):
+        pos, vel = input_line.split("@")
+        self.pos = [int(x) for x in pos.split(",")]
+        self.vel = [int(x) for x in vel.split(",")]
+
+    def pos_at_time(self, t):
+        x = self.pos[0] + (self.vel[0] * t)
+        y = self.pos[1] + (self.vel[1] * t)
+        z = self.pos[2] + (self.vel[2] * t)
+        return (x,y,z)
+
+
+    def intersects_at(self, other):
+        # Find at what t self.x = other.x
+        spx = self.pos[0]
+        opx = other.pos[0]
+        svx = self.vel[0]
+        ovx = other.vel[0]
+        pos_diff = spx - opx # How much distance is there to cover?
+        vel_diff = ovx - svx # How fast is self catching up to other?
+        if vel_diff == 0:
+            return None
+        time = pos_diff / vel_diff
+        if time < 0:
+            return None
+        
+        self_at_time = self.pos_at_time(time)
+        other_at_time = other.pos_at_time(time)
+        if math.isclose(self_at_time[1], other_at_time[1], abs_tol=100):
+            return self_at_time
+        pass
+
+def is_in_test_area(pos):
+    if pos[0] >= 200000000000000 and pos[0] <= 400000000000000:
+        if pos[1] >= 200000000000000 and pos[1] <= 400000000000000:
+            return True
+    return False
+
+def main(input_file):
+    pt1_ans = 0
+    particles = [Particle(x) for x in input_file.split("\n") if len(x) != 0]
+    for idx, p1 in enumerate(particles):
+        for p2 in particles[idx+1:]:
+            intersect = p1.intersects_at(p2)
+            if intersect is None:
+                continue
+            if is_in_test_area(intersect):
+                pt1_ans += 1
+    return (pt1_ans, 0)
+    pass
