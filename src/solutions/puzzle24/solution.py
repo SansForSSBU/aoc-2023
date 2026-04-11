29415f9587939d3a0ef990b6b0a8b20f1092cd07
Joseph Taylor <taylorjoseph3333@gmail.com>
2026-04-11 19:54:52 +0100

    WIP

--- src/solutions/puzzle24/solution.py
+++ src/solutions/puzzle24/solution.py
@@ -1,6 +1,7 @@
 import math
-from sympy import symbols, linsolve, EmptySet
+from sympy import symbols, linsolve, nonlinsolve, EmptySet, solve
 from copy import deepcopy
+import numpy as np
 
 
 t = symbols('t', real=True)
@@ -48,6 +49,50 @@ def solve_pt1(particles):
             pass
     return pt1_ans
 
+def example_problem():
+    t, t1, t2, t3, t4, t5 = symbols("t t1 t2 t3 t4 t5", real=True)
+    m, c = symbols("m c", real=True)
+    p = m*t + c
+    
+    p1 = 19 - 2*t1
+    p2 = 18 - t2
+    p3 = 20 - 2*t3
+    p4 = 12 - t4
+    p5 = 20 + t5
+
+    eqs = [
+        p.subs(t, t1) - p1,
+        p.subs(t, t2) - p2,
+        p.subs(t, t3) - p3,
+        p.subs(t, t4) - p4,
+        p.subs(t, t5) - p5
+    ]
+    finds = (t1, t2, t3, t4, t5, c, m)
+    times = solve(eqs, finds)
+    
+    diffs = [
+        times[t1] - times[t3]
+    ]
+    pass
+
+    eqs = [
+        p.subs(t, t1) - p1,
+        p.subs(t, t2) - p2,
+        p.subs(t, t3) - p3,
+        #t1 - t2 - 2, # Outside information
+        #t1 - t3 - 1 # Outside information
+    ]
+    finds = (t1, t2, t3, c, m)
+    solutions = solve(eqs, finds)
+    pass
+
+def full_problem(particles):
+    t_values = np.array([part.v[0] for part in particles])
+    p_values = np.array([part.p[0] for part in particles])
+    
+    m, c = np.polyfit(t_values, p_values, 1)
+    pass
+
 def main(input_file):
     particles = [Particle(x) for x in input_file.split("\n") if len(x) != 0]
     for idx, particle in enumerate(particles):
@@ -55,17 +100,23 @@ def main(input_file):
         time_sym = symbols(f"t{idx+1}", real=True)
         particle.set_self_pos(time_sym)
     pt1_ans = 0#solve_pt1(particles)
-    eqs = [particle.x for particle in particles]
+    example_problem()
+    #eqs = [particle.x for particle in particles]
+    pass
+    full_problem(particles)
     destroyer_m = symbols("des_m", real=True)
     destroyer_c = symbols("des_c", real=True)
     destroyer_t = symbols("des_t", real=True)
-    destroyer_eq = destroyer_m * destroyer_t + destroyer_c
+    destroyer_eq = (destroyer_m * destroyer_t) + destroyer_c
     dest_eqs = []
+    time_coefficients = []
     for first_eq in eqs[:5]:
         t = list(first_eq.free_symbols)[0]
+        time_coefficients.append(t)
         dest_eq = destroyer_eq.subs(destroyer_t, t)
+        pass
         dest_eqs.append(dest_eq - first_eq)
-    a = linsolve(dest_eqs, (destroyer_m, destroyer_c))
     pass
+    
     
     return (pt1_ans, 0)
