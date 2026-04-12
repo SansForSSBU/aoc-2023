b0197dfd46c17e83c0ef254d93bf2d3feb1a3ec3
Joseph Taylor <taylorjoseph3333@gmail.com>
2026-04-12 11:32:35 +0100

    Clean up puzzle 25

--- src/solutions/puzzle25/solution.py
+++ src/solutions/puzzle25/solution.py
@@ -1,15 +1,5 @@
 import networkx as nx
 
-def get_num_connections(connections, node_name):
-    n = 0
-    for conn in connections:
-        if node_name in conn:
-            n += 1
-    return n
-
-def find_paths_between(n1, n2, connections):
-    pass
-
 def main(input_file):
     lines = [l for l in input_file.split("\n") if len(l) > 0]
     d = {}
@@ -18,15 +8,9 @@ def main(input_file):
         d[k] = v.split(" ")
 
     G = nx.Graph()
-    connections = []
-    nodes = []
     for k, v in d.items():
         for k2 in v:
             G.add_edge(k, k2)
-            if not k in nodes:
-                nodes.append(k)
-            if not k2 in nodes:
-                nodes.append(k2)
 
     edges_to_remove = nx.minimum_edge_cut(G)
     G.remove_edges_from(edges_to_remove)
